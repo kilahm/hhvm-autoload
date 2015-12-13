4eebf5380a21b1fab04709ad9cf0e8c57b296184
Fred Emmott <github@fredemmott.co.uk>
2015-12-13 22:46:54 +0000

    Add option to crawl vendor or not

--- src/Config.php
+++ src/Config.php
@@ -5,5 +5,6 @@ namespace FredEmmott\AutoloadMap;
 type Config = shape(
   'autoloadFilesBehavior' => AutoloadFilesBehavior,
   'composerJsonFallback' => bool,
+  'includeVendor' => bool,
   'roots' => ImmVector<string>,
 );
